c696a4e9fae3ee7f788e0f125d1d7340420a69ab
Allen Douglas <allenissabigboi@gmail.com>
2024-07-01 15:16:52 +0100

    Created add todo logic with model file, hook and usestate

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,15 +1,31 @@
 import React, { useState } from 'react';
 import InputField from './components/inputField';
+import TodoList from './components/todoList';
 import './App.css';
+import { Todo } from './model';
 
 const App : React.FC = () => {
-  const [todo, setstate] = useState("")
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  // console.log(todo)
 
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo, isDone: false}]);
+      setTodo("");
+    }
+
+  };
 
   return (
     <div className="App">
      <span className="heading"> Task App</span>
-     <InputField />
+     <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+     <TodoList />
+
     </div>
   );
 }
--- typescript-tutorial/src/components/inputField.tsx
+++ typescript-tutorial/src/components/inputField.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./styles.css"
 
-const InputField = () => {
-  return (
-  <form className="input-form">
-    <input placeholder="Enter a task" type="text" className='input-box'/>
-    <button className='submit-button' type="submit">Go</button>
-  </form>
-  );
+type Props = {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void; // handleAdd is a function and it doesent return anything, the function was defined in the app.tsx file to handle to-do add
+}
+
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+    const inputref = useRef<HTMLInputElement>(null);
+
+
+    return (
+        <form className="input-form" onSubmit={(e) => {
+            handleAdd(e);
+            inputref.current?.blur(); // This line removes the focus blur of the input field
+
+        }} // On submit of the form the "handleadd" fuction is called and here i have added the event parameter "e"
+        >
+            <input
+                value={todo}
+                ref={inputref}
+                onChange={(e) => setTodo(e.target.value)}
+                placeholder="Enter a task"
+                type="text"
+                className='input-box' />
+            <button className='submit-button' type="submit">Go</button>
+        </form>
+    );
 }
 
 export default InputField
